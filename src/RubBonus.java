de46fd037f1e60c4e7b4afa2a06677dae4f3b4b2
Sizova-QA88 <nataliasiz19@bk.ru>
2024-07-09 23:06:46 +0300

    Update commit

--- src/RubBonus.java
+++ src/RubBonus.java
@@ -6,18 +6,17 @@ public class RubBonus {
         int balanceNow = 100; //начальный счет
         int refill = 1000; //пополнение
         int bonus; // бонусные рубли
-        int resultBalance1;
-        int resultBalance2;
+        int resultBalance;
 
         //Производим расчет, с учетом порога пополнения
 
         if (refill > 1000) {
             bonus = (refill / 100);
-            resultBalance1 = (balanceNow + refill + bonus);
-            System.out.println("Начальная сумма: " + balanceNow + ", " + "Сумма пополнения: " + refill + ", " + "Бонус: " + bonus + ", Итоговая сумма: " + resultBalance1);
+            resultBalance = (balanceNow + refill + bonus);
+            System.out.println("Начальная сумма: " + balanceNow + ", " + "Сумма пополнения: " + refill + ", " + "Бонус: " + bonus + ", Итоговая сумма: " + resultBalance);
         } else {
-            resultBalance2 = (balanceNow + refill);
-            System.out.println("Начальная сумма: " + balanceNow + ", " + "Сумма пополнения: " + refill + ", " + "Итоговая сумма: " + resultBalance2);
+            resultBalance = (balanceNow + refill);
+            System.out.println("Начальная сумма: " + balanceNow + ", " + "Сумма пополнения: " + refill + ", " + "Итоговая сумма: " + resultBalance);
         }
     }
 }
